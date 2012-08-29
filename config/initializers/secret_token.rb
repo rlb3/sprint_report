4769f01fb51b173f56e6d461cad90470616f0ac1
Robert Boone <robert.l.boone@gmail.com>
2012-08-29 10:14:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SprintReport::Application.config.secret_token = 'f3fc393144f100ad464c249382837b0a6018f9fb09f45b19907cd1fe3729a9ecf298dd5cd84fad64a1c1e7dbc9a93b86f54e6473815ab3267c36948d34f5768a'
