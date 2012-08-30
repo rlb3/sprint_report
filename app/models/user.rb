1d29d5931ad820b263a267a533e112dcbe767712
Robert Boone <robert.l.boone@gmail.com>
2012-08-30 09:29:43 -0500

    Add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :percent, :sprint_id, :role
   belongs_to :sprint
+
+  validates :name, :percent, :sprint_id, :role, :presence => true
+  validates :percent,
+            :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
 end
